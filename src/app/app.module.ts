a2e4f92fa1d92ee65cdc97a8852c193c5d9709d1
DESKTOP-I0KBJ96\marin <marino_0104@hotmail.com>
2022-02-17 23:22:59 -0500

    cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     PagesModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
